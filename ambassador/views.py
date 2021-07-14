8f7c645553b67229c3be304c271753a5e81843f0
ylu <ylu@ert.com>
2021-07-13 23:51:19 -0400

    add product url

--- ambassador/views.py
+++ ambassador/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from core.models import Product
+from .serializers import ProductSerializer
+
+
+class ProductFrontendAPIView(APIView):
+
+    def get(self, _):
+      products = Product.objects.all()
+      serializer = ProductSerializer(products, many=True)
+      return Response(serializer.data)
+
+
+class ProductBackendAPIView(APIView):
+
+    def get(self, _):
+      products = Product.objects.all()
+      serializer = ProductSerializer(products, many=True)
+      return Response(serializer.data)
+
+    
