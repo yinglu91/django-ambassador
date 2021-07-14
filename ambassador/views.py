0150d0f6ffbc881f7666513e483019d6b4bb8472
ylu <ylu@ert.com>
2021-07-14 14:43:39 -0400

    add cache

--- ambassador/views.py
+++ ambassador/views.py
@@ -1,5 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 
 from core.models import Product
 from .serializers import ProductSerializer
@@ -8,7 +10,7 @@ import time
 
 
 class ProductFrontendAPIView(APIView):
-
+    @method_decorator(cache_page(60*60*2, key_prefix='products_frontend'))  # 2 hours
     def get(self, _):
       products = Product.objects.all()
       serializer = ProductSerializer(products, many=True)
@@ -20,7 +22,7 @@ class ProductBackendAPIView(APIView):
     def get(self, _):
       products = cache.get('products_backend')
       if not products:
-        time.sleep(2) # 2 seconds
+        time.sleep(2) # 2 seconds for mimic the delay
         products = list(Product.objects.all())
         cache.set('products_backend', products, timeout=60*30)  # 30 minutes
 
