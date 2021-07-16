0c26cc1ebadfba4a332446688f940ef5bda7c1d7
ylu <ylu@ert.com>
2021-07-15 23:45:17 -0400

    add db transaction

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,4 @@
+from django.utils import translation
 from administrator.views import AmbassadorAPIView
 from rest_framework import serializers, exceptions
 from rest_framework.permissions import IsAuthenticated
@@ -6,6 +7,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
+from django.db import transaction
 
 from core.models import Link, Product, Order,OrderItem, User
 from .serializers import LinkSerializer, ProductSerializer, UserSerializer
@@ -24,6 +26,8 @@ class LinkAPIView(APIView):
 
     
 class OrderAPIView(APIView):
+
+  @transaction.atomic
   def post(self, request):
     data =  request.data
     link = Link.objects.filter(code=data['code']).first()
@@ -31,33 +35,47 @@ class OrderAPIView(APIView):
     if not link:
       raise exceptions.APIException('Invalid code!')
 
-    order = Order()
-    order.code = link.code
-    order.user_id = link.user.id
-    order.ambassador_email = link.user.email
-    order.first_name = data['first_name']
-    order.last_name = data['last_name']
-    order.email = data['email']
-    order.address = data['address']
-    order.country = data['country']
-    order.city = data['city']
-    order.zip = data['zip']
-    order.save()
+    try:
+      order = Order()
+      order.code = link.code
+      order.user_id = link.user.id
+      order.ambassador_email = link.user.email
+      order.first_name = data['first_name']
+      order.last_name = data['last_name']
+      order.email = data['email']
+      order.address = data['address']
+      order.country = data['country']
+      order.city = data['city']
+      order.zip = data['zip']
 
-    for item in data['products']:
-        product = Product.objects.filter(pk=item['product_id']).first()
-        quantity = decimal.Decimal(item['quantity'])
+      with transaction.atomic():
+        order.save()
 
-        order_item = OrderItem()
-        order_item.order = order
-        order_item.product_title = product.title
-        order_item.price = product.price
-        order_item.quantity = quantity
-        order_item.ambassador_revenue = decimal.Decimal(.1) * product.price * quantity
-        order_item.admin_revenue = decimal.Decimal(.9) * product.price * quantity
-        order_item.save()
+      for item in data['products']:
+          product = Product.objects.filter(pk=item['product_id']).first()
+          quantity = decimal.Decimal(item['quantity'])
+
+          order_item = OrderItem()
+          order_item.order = order
+          order_item.product_title = product.title
+          order_item.price = product.price
+          order_item.quantity = quantity
+          order_item.ambassador_revenue = decimal.Decimal(.1) * product.price * quantity
+          order_item.admin_revenue = decimal.Decimal(.9) * product.price * quantity
+
+          with transaction.atomic():
+            order_item.save()
+
+      
+      return Response({
+        'message': 'success'
+      })
+
+        
+    except Exception:
+      transaction.rollback()
 
 
     return Response({
-      'message': 'success'
+      'message': 'Error occurred.'
     })
